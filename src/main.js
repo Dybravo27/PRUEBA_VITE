929c57909bff0240ab7d81f0e1ab814fb9bd0c72
Dylan Bravo <dylanslebyng@gmail.com>
2025-05-30 17:03:09 -0500

    Pruebas Finalizadas

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import 'sweetalert2/src/sweetalert2.scss'
 import 'animate.css';
 
 
-
 const div = document.querySelector("#app");
 const div2 = document.createElement("div");
 div2.classList.add("app-content");
@@ -18,29 +17,49 @@ span.textContent = "0";
 
 const boton_sumar = document.createElement("button");
 boton_sumar.classList.add("button");
-boton_sumar.textContent = "+";
+boton_sumar.classList.add("button__sumar");
+const icon_sumar = document.createElement("i");
+icon_sumar.classList.add("ri-add-large-line");
 
 const boton_restar = document.createElement("button");
 boton_restar.classList.add("button");
-boton_restar.textContent = "-";
+boton_restar.classList.add("button__restar");
+const icon_restar = document.createElement("i");
+icon_restar.classList.add("ri-subtract-line");
 
-const boton = document.createElement("button");
-boton.classList.add("button");
-boton.textContent = "RESTABLECER";
 
-div_content.append(boton_sumar, boton_restar, boton);
-div_span.append(span);
+const boton_restablecer = document.createElement("button");
+boton_restablecer.classList.add("button");
+boton_restablecer.classList.add("button__restablecer");
+const icon_restablecer = document.createElement("i");
+icon_restablecer.classList.add("ri-refresh-line");
+
 div.append(div2);
 div2.append(div_span, div_content);
+div_span.append(span);
+boton_sumar.append(icon_sumar);
+boton_restar.append(icon_restar);
+boton_restablecer.append(icon_restablecer);
+div_content.append(boton_sumar, boton_restar, boton_restablecer);
 
-const contador = document.querySelector("div#app > div > div > span");
+// const contador = document.querySelector("div#app > div > div > span");
+const contador = document.querySelector(".text_span");
 
-boton_sumar.addEventListener("click", () => {
-  contador.textContent = sumar();
+boton_sumar.addEventListener("click", (e) => {
+  if (e.target.closest(".button__sumar"))
+  {
+    contador.textContent = sumar();
+  }
 });
-boton_restar.addEventListener("click", () => {
-  contador.textContent = restar();
+boton_restar.addEventListener("click", (e) => {
+  if (e.target.closest(".button__restar"))
+  {
+    contador.textContent = restar();
+  }
 });
-boton.addEventListener("click", () => {
-  contador.textContent = restablecer();
+boton_restablecer.addEventListener("click", (e) => {
+  if (e.target.closest(".button__restablecer"))
+  {
+    contador.textContent = restablecer();
+  }
 });
